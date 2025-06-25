aa24b24266577f356f933828755d4c1fd3b2b52a
aweitz1 <annweitz@posteo.net>
2025-06-25 22:00:17 +0200

    Adding more comments to code

--- extract_bee_crops.py
+++ extract_bee_crops.py
@@ -3,21 +3,30 @@ import cv2
 
 
 
-# Dictionary to store detections grouped by (recording_no, frame_no)
 
 
 
 def extract_trajectory_information(trajectories_path):
+    """
+    Given a path to a folder containing txt. files with trajectory information, extracts this information
+    and saves it in a dictionary.
+    :param trajectories_path: Path to the folder containing the trajectory files
+    :return: A dictionary containing detection information, grouped by (recording_no, frame_no). Individual bees are
+    stored in a dictionary containing bee_no, pos_x, pos_y, class_no, angle.
+    """
 
+    # Dictionary to store detections grouped by (recording_no, frame_no)
     detections_by_frame = {}
 
     for root, dirs, files in os.walk(trajectories_path):
         for file in files:
             if file.endswith('.txt'):
-                rec_no = root[-1]  # or extract recording_no appropriately
+
+                rec_no = root[-1]
                 bee_no = file.replace(".txt", "")
                 file_path = os.path.join(root, file)
                 try:
+                    # Open .txt file and extract all information
                     with open(file_path, 'r', encoding='utf-8') as f:
                         for line in f:
                             line_parts = line.strip().split(",")
@@ -43,12 +52,19 @@ def extract_trajectory_information(trajectories_path):
 
 
 def extract_frames_from_video(videos_path, frames_path):
-
+    """
+    Extracts all frames from all videos in the given video_path and saves them to frames_path.
+    In case the folder for that recording already exists, that video gets skipped.
+    :param videos_path: Path to the folder containing the videos.
+    :param frames_path: Path to the folder the frame images are supposed to be saved in.
+    """
     for root, dirs, files in os.walk(videos_path):
         for file in files:
             if file.endswith('.mp4'):
                 video_path = os.path.join(root, file)
                 rec_no = file.replace(".mp4", "")[-1]
+
+                # If folder already exists, assume that all frames were successfully extracted, might change this later to be more robust
                 if os.path.isdir(frames_path + "rec" + rec_no):
                     print(f"Skipping video {file}")
                 else:
@@ -58,7 +74,7 @@ def extract_frames_from_video(videos_path, frames_path):
                     success = True
                     while success:
                         success, image = vidcap.read()
-                        cv2.imwrite(f"{frames_path}/rec{rec_no}/frame%04d.png" % count, image)  # save frame as JPEG file
+                        cv2.imwrite(f"{frames_path}/rec{rec_no}/frame%04d.png" % count, image)
                         count += 1
 
 
@@ -76,7 +92,6 @@ def crop_bee(img, x, y, crop_size=256):
     Returns:
         numpy.ndarray: Cropped image of shape (crop_size, crop_size)
     """
-    # Load image in grayscale
     if img is None:
         raise ValueError("Image not found or failed to load.")
 
@@ -111,7 +126,18 @@ def crop_bee(img, x, y, crop_size=256):
 
 
 def extract_save_crops(bee_detections_by_frame, frames_path,crops_path, bee_number = None):
+    """
+    Given a dictionary of bee detections, goes through all frames and crops all bees for that frame. If bee_number is specified, only extracts crops for that bee.
+    :param bee_detections_by_frame: Dictionary of bee detections
+    :param frames_path: Path to folder containing the frames
+    :param crops_path: Path to save the cropped images to
+    :param bee_number: Optional, to extract crops for only one bee. For testing purposes
+    """
+
+    # Make sure that crop dir exists
     os.makedirs(crops_path, exist_ok=True)
+
+    # Loop through all (rec_no, frame_no) pairs and extract all (or the single one) bees for that frame, so frames only have to be opened once each
     for (rec_no, frame_no), bees in sorted(bee_detections_by_frame.items()):
         frame_name = f"frame{str(frame_no).rjust(4, '0')}.png"
         frame_path = os.path.join(frames_path, f"rec{rec_no}", frame_name)
@@ -148,7 +174,7 @@ def main():
     bee_detections_by_frame = extract_trajectory_information(trajectories_path)
     extract_frames_from_video(vid_path, frames_path)
 
-    extract_save_crops(bee_detections_by_frame, frames_path,crops_path ,"000022")
+    extract_save_crops(bee_detections_by_frame, frames_path,crops_path ,"000000")
 
     pass
 
