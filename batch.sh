794f5146571d9505d9b1cf74dec280fd47db6f9e
Tom Hunze <dev@thunze.de>
2025-06-30 19:57:44 +0200

    Add example Slurm batch script

---
+++ batch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH --partition=gpu
+#SBATCH -G h100:1
+#SBATCH --mem=128gb
+#SBATCH --time=0:30:00
+
+set -e
+set -u
+set -o pipefail
+set -x
+
+pip install uv==0.7.13
+uv run honeybee-learning --model simclr --wandb
