dafeba5d66a6b13d70841f9e8266a73142783090
aweitz1 <annweitz@posteo.net>
2025-07-12 17:49:53 +0200

    Adding final comments to code

--- honeybee_learning/image_transformations.py
+++ honeybee_learning/image_transformations.py
@@ -7,6 +7,15 @@ from torchvision import transforms
 __all__ = ["crop_resize_flip"]
 
 def random_transform(img):
+    """
+    Apply the following transformations to the input image:
+        * Random cropping and resizing to the original size
+        * Random vertical flip (50% chance)
+        * Random horizontal flip (50% chance)
+        * Random rotation (0, 90, 180 or 270°)
+    :param img: The input image.
+    :return: The transformed output image.
+    """
     # Random crop parameters for this image
     i, j, h, w = transforms.RandomCrop.get_params(img, output_size=(128, 128))
     img = F.crop(img, i, j, h, w)
@@ -29,6 +38,15 @@ def random_transform(img):
     return img
 
 def crop_resize_flip(image_pair):
+    """
+    Applies the following transformations to a pair of input images:
+        * Random cropping and resizing to the original size
+        * Random vertical flip (50% chance)
+        * Random horizontal flip (50% chance)
+        * Random rotation (0, 90, 180 or 270°)
+    :param image_pair: The input image pair as a tuple of tensors.
+    :return: The transformed images as a tuple of tensors.
+    """
     img1, img2 = image_pair
     img1 = random_transform(img1)
     img2 = random_transform(img2)
