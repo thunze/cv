cf4c863e5c2493d97bcfe0aeab195492ec6eca6a
Tom Hunze <dev@thunze.de>
2025-07-13 11:10:10 +0200

    Add example batch scripts for linear testing

---
+++ batch-test-linear-simclr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH --partition=gpu
+#SBATCH -G h100:1
+#SBATCH --mem=512gb
+#SBATCH --time=1:00:00
+
+set -e
+set -u
+set -o pipefail
+set -x
+
+pip install uv==0.7.13
+uv run test-linear --wandb /scratch/cv-course2025/group7/representations/simclr_20250704_213650_epoch_90_representations_2048_first_relu.npy
